6ac4813c889589fc7fc11d355423b43c666e0b4e
Maria <mariaferagui@gmail.com>
2019-01-21 16:02:44 +0100

    Mi commit

---
+++ duckduckgoose.py
@@ -0,0 +1,10 @@
+def duckduckgoose (gente, posicion):
+    lista = list (gente)
+    posicion = int (posicion)
+    oca = lista[posicion]
+    return oca, posicion
+
+oca = duckduckgoose (['María', 'Alicia', 'Blanca', 'Celia','Espe'], 4)[0]
+posicion = duckduckgoose (['María', 'Alicia', 'Blanca', 'Celia','Espe'], 4)[1]
+
+print ('La oca es: ', oca, 'y su posición es: ', posicion)
